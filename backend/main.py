d1e8db4c8d1236b7278fdbaddfa93f824ab21a1a
David <53689532+RelativelyFine@users.noreply.github.com>
2022-01-28 12:26:24 -0400

    get_component made

--- backend/main.py
+++ backend/main.py
@@ -4,3 +4,7 @@ from pcpartpicker import API
 class Components:
     def __init__(self, region='ca'):
         self.api = API(region)
+
+    def get_component_json(self, component: str):
+        component_data = self.api.retrieve(component)
+        return component_data
