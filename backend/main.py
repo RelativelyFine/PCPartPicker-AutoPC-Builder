470ba706cfadd59afe29580b8f02878e4791080e
jrai51 <87856634+jrai51@users.noreply.github.com>
2022-01-29 21:39:05 -0500

    Added component based on price range

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,38 @@
-from pcpartpicker import API
+from multiprocessing import cpu_count
+from pcpartpicker import API 
+from decimal import Decimal
 
+
+api = API()
 
 class Components:
-    def __init__(self, region='ca'):
+    def __init__(self, part, region='ca'):
         self.api = API(region)
+        self.supported_parts = api.supported_parts
+        self.part = part 
+        self.items = self.api.retrieve(part)
+        
+        
+
 
     def get_component_json(self, component: str):
-        component_data = self.api.retrieve(component)
-        return component_data
+        return self.items
+
+    def get_items_p(self, lower, upper):
+        ''' Get items from price '''
+        comps = []
+        for item in self.items[self.part]:
+           if item.price.amount <= Decimal(upper) and item.price.amount >= Decimal(lower):
+                if len(comps) < 5:
+                    comps.append(item)
+                else:
+                    break
+        return comps
+
+    
+        
 
 
-cpu = Components()
+cpu = Components('motherboard')
 
-cpudata = cpu.get_component_json("cpu")
+print(cpu.get_items_p(500, 900))
