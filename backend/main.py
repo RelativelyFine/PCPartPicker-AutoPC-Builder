12914d5441dc98062d8cc228936e384bdbee2208
jrai51 <87856634+jrai51@users.noreply.github.com>
2022-01-29 22:32:19 -0500

    ratio of prices to return list of components

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,19 @@
 from multiprocessing import cpu_count
+from traceback import print_exception
 from pcpartpicker import API 
 from decimal import Decimal
+import csv
 
 
 api = API()
 
-class Components:
-    def __init__(self, part, region='ca'):
+class Component:
+    def __init__(self, part, price, region='ca'):
         self.api = API(region)
         self.supported_parts = api.supported_parts
         self.part = part 
         self.items = self.api.retrieve(part)
+        self.price = price
         
         
 
@@ -18,8 +21,11 @@ class Components:
     def get_component_json(self, component: str):
         return self.items
 
-    def get_items_p(self, lower, upper):
-        ''' Get items from price '''
+    def get_items_p(self):
+        ''' Get items from price range '''
+        upper = self.price * 1.25
+        lower = self.price * 0.75
+    
         comps = []
         for item in self.items[self.part]:
            if item.price.amount <= Decimal(upper) and item.price.amount >= Decimal(lower):
@@ -33,6 +39,24 @@ class Components:
         
 
 
-cpu = Components('motherboard')
+cpu = Component('motherboard', 200)
 
-print(cpu.get_items_p(500, 900))
+
+#print(cpu.supported_parts)
+#get components based on component price ratio 
+ 
+ratio = {'motherboard': 0.15, 'cpu': 0.15, 'video-card': 0.50, 'memory': 0.10}
+
+parts_list = {}
+
+def get_parts(price):
+    for key in ratio:
+        print(key)
+        part = Component(key, price*ratio[key])
+        parts_list[key] = part.get_items_p()
+
+get_parts(2000)
+
+f = open("out.txt", "w")
+f.writelines(parts_list)
+f.close()
